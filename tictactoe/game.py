8a726a01dfbc4653d526bd0a1064b909a3f1514e
Joel Kram <joelnskram@gmail.com>
2024-12-04 15:26:23 +0000

    python excercises

---
+++ tictactoe/game.py
@@ -0,0 +1,14 @@
+import random
+import math
+
+# create board
+
+class Board:
+    def __init__(self, board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]):
+        self.board = board
+        
+    def createBoard():
+        print("1 ")
+
+
+  
